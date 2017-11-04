23b9a9594817caac01214c9a285468b9f2ce7381
azuldev1 <zuma3434@gmail.com>
2017-11-04 15:18:45 -0500

    nav toggle

--- js/customScript.js
+++ js/customScript.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
+        if($("#wrapper").hasClass("toggled")){
+            $("#menuIcon i").toggleClass("fa-bars fa-times");
+        } else {
+            $("#menuIcon i").toggleClass("fa-times fa-bars");
+        };
     });
     
 });
